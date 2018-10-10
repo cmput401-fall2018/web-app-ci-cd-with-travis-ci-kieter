7dc97b166513beabcd0575368893089c63452aac
kieter <balisnom@ualberta.ca>
2018-10-09 23:00:31 -0600

    Started work in slides

---
+++ venvas3/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kieter/Google Drive (balisnom@ualberta.ca)/University/CMPUT 401/web-app-ci-cd-with-travis-ci-kieter/venvas3/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
